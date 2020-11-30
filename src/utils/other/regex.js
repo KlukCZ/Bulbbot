bb87d4df857726301a2ce70c63ec92a9c62e7918
y3ll0wlife <y3ll0wlife@protonmail.com>
2020-11-30 22:29:30 +0100

    Found more hehe

--- src/utils/other/regex.js
+++ src/utils/other/regex.js
@@ -2,3 +2,4 @@ module.exports.USER_MENTION = /<@?!?[0-9>]+/g;
 module.exports.USER_MENTION_USER_ID = /<@?!?[0-9>]+|[0-9>]{17,}/g;
 module.exports.ROLE_MENTION = /<@&[0-9>]+/g;
 module.exports.ATEVERYONE_ATHERE = /@everyone|@here+/g;
+module.exports.FIND_DIGITS = /\D/g;
