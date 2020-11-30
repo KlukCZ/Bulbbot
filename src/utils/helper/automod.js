48bac5a0d3b464b63113b01d2b7bcafd0eebf810
KlukCZ <l.neumajer@seznam.cz>
2020-11-30 17:35:22 +0100

    Some automod preparation
    Very crude lol, not hooked to the db, compares allowed invites to a locally stored array. Pls fix kthx

--- src/utils/helper/automod.js
+++ src/utils/helper/automod.js
@@ -1,29 +1,30 @@
+const Log = require("../moderation/log")
+
+function checkForInvites(string) {
+	let allowed_invites = ["allowed_invite", "kluk", "philip"]
+	for (let i = 0; i < allowed_invites.length; i++) {
+		let regex = new RegExp("(http:\/\/|https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/(" + allowed_invites[i] + ")+", "gi")
+		if (string.match(regex)) {
+			return false;
+		}
+	}
+ 	return string.match(/(http:\/\/|https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([A-Za-z0-9])+/g);
+}
+
 module.exports = {
-	Master: async (client, string, guild) => {
-		return string;
+	Master: async (client, string, guild, message) => {
+		if(checkForInvites(string)) {
+			message.delete();
+			/*
+			* Please log this as a separate action, not Mod_Action. That way the user has control over what channel it gets logged to.
+			* Can be left as Mod_Action as well but I'll log twice
+			 */
+			await Log.Mod_action(
+				client,
+				guild,
+				`${message.author.username} has violated the \`INVITE\` filter in ${message.channel}: \n**Content:** ${message.content}`,
+				""
+			);
+		}
 	},
 };
-
-/*
-Regex
-/ (http:\/\/|https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/ /g
-/ (http:\/\/|https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([A-Za-z0-9])+ /g
-
-Invitelinks:
-
-Links:
-
-
-abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789 !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-
-
-
-https://discord.gg/6gdSVg5a
-http://discord.gg/6gdSVg5a
-discord.gg/6gdSVg5a
-discord.gg/6gdSVg5a
-https://discord.com/invite/6gdSVg5a
-https://discordapp.com/invite/6gdSVg5a
-http://discordapp.com/invite/6gdSVg5a
-http://discord.com/invite/6gdSVg5a
-*/
